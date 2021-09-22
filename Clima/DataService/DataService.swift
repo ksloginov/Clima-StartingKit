7f016edc86445267cf75c7e0e59649d8efd1e135
Konstantin Loginov <loginov.k@gmail.com>
2021-09-22 20:08:37 +0200

    Starting kit

---
+++ Clima/DataService/DataService.swift
@@ -0,0 +1,17 @@
+//
+//  DataService.swift
+//  Clima
+//
+//  Created by Konstantin Loginov on 22.09.2021.
+//
+
+import Foundation
+
+struct DataService {
+    
+    private static let host = "https://api.openweathermap.org/data/2.5/weather"
+    
+    // ?q=Oslo&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric
+    // ?lat=\(latitude)&lon=\(longitude)&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric
+    
+}
---
+++ Clima/Model/WeatherCondition.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherCondition.swift
+//  Clima
+//
+//  Created by Konstantin Loginov on 22.09.2021.
+//
+
+import Foundation
+
+struct WeatherCondition {
+    
+    
+    /*
+     switch conditionId {
+     case 200...232:
+         return "cloud.bolt"
+     case 300...321:
+         return "cloud.drizzle"
+     case 500...531:
+         return "cloud.rain"
+     case 600...622:
+         return "cloud.snow"
+     case 701...781:
+         return "cloud.fog"
+     case 800:
+         return "sun.max"
+     case 801...804:
+         return "cloud.bolt"
+     default:
+         return "cloud"
+     }
+     
+     */
+}
