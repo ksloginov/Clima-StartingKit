04cbf542768301f6ba74ef35cd83fc1f55a7ac90
Konstantin Loginov <loginov.k@gmail.com>
2021-09-22 19:13:23 +0200

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Konstantin Loginov on 22.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
