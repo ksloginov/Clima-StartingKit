e0f198a669d6b98cd9bb89221761250ed7984c3c
Konstantin Loginov <loginov.k@gmail.com>
2021-09-22 20:20:17 +0200

    Update DataService.swift

--- Clima/DataService/DataService.swift
+++ Clima/DataService/DataService.swift
@@ -11,7 +11,7 @@ struct DataService {
     
     private static let host = "https://api.openweathermap.org/data/2.5/weather"
     
-    // ?q=Oslo&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric
-    // ?lat=\(latitude)&lon=\(longitude)&appid=9067ea08c6a8b9bce1efaf060de4246c&units=metric
+    // ?q=Oslo&appid=<insertkey>&units=metric
+    // ?lat=\(latitude)&lon=\(longitude)&appid=<insertkey>&units=metric
     
 }
